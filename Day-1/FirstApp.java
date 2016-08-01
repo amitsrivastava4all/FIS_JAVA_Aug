352094aafdda3e97efce73de9e636fcec327b588
amit4alljava <amit4alljava@gmail.com>
2016-08-01 17:21:30 +0530

    Codes

---
+++ Day-1/FirstApp.java
@@ -0,0 +1,49 @@
+
+// class name must start with Alphabet or with _ and $ and should be in Capital
+// Use _ and $ as a Special Char
+// Class name doesn't have space
+// It can be AlphaNumeric
+strictfp class SecondApp
+{
+public static void main(){
+System.out.println("this is My Main ");
+}
+  public static void main(String ...args){
+// public - It is a scope
+// It is Access Outside the Package
+// static - One Copy in a Memory
+// It is get loaded when class is loaded.
+// String - It is a predefine class and it is the replacement of char []
+// args is a variable
+// String args[] - Command Line Arguments
+//BigInteger q ;
+main();
+String p = "Hello";// 1 or 0 object
+String q = new String("Hello"); // 2 or 1 object
+//String q = p;
+if(p==q){
+System.out.println("Same Ref");
+}
+else
+{
+System.out.println("Not Same Ref ");
+}
+if(p.equals(q)){
+System.out.println("Same Value ");
+}
+else{
+System.out.println("Not Same Value ");
+}
+//float q = 90.20f;
+int a=100 ;
+int b=200 ;
+int c = a + b;
+byte x = (byte)130;
+System.out.println("Sum is  "+c+" X is "+x);
+// System is a predefine class 
+// and out is a predefine object and it is a object of
+// PrintStream class
+// println() is a method and it is coming from PrintStream class
+// print() to print in a same line  and println() is for new line 
+}
+}
